a694ded393a3917bb70533ffbcbac20b43964fb7
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-01-07 01:49:31 -0300

    atualizando o projeto

---
+++ js/index.js
@@ -0,0 +1,13 @@
+	var div = document.querySelector('.apresentacao-titulo');
+		var texto = 'Olá, eu sou o Matheus';
+
+		function escrever(str, el) {
+			var char = str.split('').reverse();
+			var typer = setInterval(function() {
+				if (!char.length) return clearInterval(typer);
+				var next = char.pop();
+				el.innerHTML += next;
+			}, 100);
+		}
+
+		escrever(texto, div);
