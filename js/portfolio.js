ce369ddc497e619364958eeb21adbc93c85c4253
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-02-12 01:44:12 -0300

    Criando a sessão portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,23 @@
+
+
+$("#acap").click(acap);
+function acap() {
+
+	
+	$("#acap-descricao").slideToggle(600);
+}
+$("#alurinha").click(alura);
+function alura() {
+	
+	$("#alurinha-descricao").slideToggle(600);
+}
+$("#calculadora-depreciacao").click(calculadora);
+function calculadora() {
+	
+	$("#calculadora-depreciacao-descricao").slideToggle(600);
+}
+$("#aparecida-nutricao").click(aparecida);
+function aparecida() {
+	
+	$("#aparecida-nutricao-descricao").slideToggle(600);
+}
